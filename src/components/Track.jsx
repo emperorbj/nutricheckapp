3dcf0f1d7b9f3abb97037f0c8aee7ed78c728a3e
BOLAJI OPATOLA <138551605+emperorbj@users.noreply.github.com>
2024-03-28 19:28:54 -0700

    Delete src directory

--- src/components/Track.jsx
+++
@@ -1,6 +0,0 @@
-export default function Track()
-{
-    return(
-        <h1>Welcome page</h1>
-    )
-}
--- src/components/contexts/UserContext.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-// createContext creates a global variable that can be accessed by any component
-export const UserContext = createContext()
